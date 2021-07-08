8f38698fe08e77c5dc60ff29471702c9c5bccb6a
Omarius <70237021+credleo95@users.noreply.github.com>
2021-07-07 23:03:09 -0400

    gserver built and get request working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,23 @@
 // BUILD YOUR SERVER HERE
+const express = require('express'); 
+const app = express(); 
+module.exports = app // EXPORT YOUR SERVER instead of {}
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+const User = require('./users/model');
+
+
+
+app.use(express.json()); 
+
+app.post('/api/users', async (req, res) => {
+const newUser = req.body; 
+await User.create(newUser); 
+
+res.status(201).json(newUser)
+});
+
+app.get('/api/users', async (req, res) => {
+    const users = await User.find();
+    res.json(users)
+});
