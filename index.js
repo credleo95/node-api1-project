30bd571b49c7999e4d29b6bac1fa5a8765cb3b12
Omarius <70237021+credleo95@users.noreply.github.com>
2021-07-16 02:06:13 -0400

    back to normal

--- index.js
+++ index.js
@@ -7,15 +7,7 @@ const port = process.env.PORT || 5000;
 server.use('/', (req, res) => {
   const messageOfTheWeek =
     process.env.MOTW || 'Waiting on a message for the week!';
-  res.send(`API Server is here to stay! ${messageOfTheWeek}
-  | Method | URL            | Description                                                                                            |
-| ------ | -------------- | ------------------------------------------------------------------------------------------------------ |
-| POST   | /api/users     | Creates a user using the information sent inside the request body.                                   |
-| GET    | /api/users     | Returns an array users.                                                                                |
-| GET    | /api/users/:id | Returns the user object with the specified id.                                                       |
-| DELETE | /api/users/:id | Removes the user with the specified id and returns the deleted user.                                 |
-| PUT    | /api/users/:id | Updates the user with the specified id using data from the request body. Returns the modified user |
-  `);
+  res.send(`API Server is here to stay! ${messageOfTheWeek}`);
 });
 server.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
