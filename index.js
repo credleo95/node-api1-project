2480feaa62e0feb4e6654d920aafc1bf70082e39
Omarius <70237021+credleo95@users.noreply.github.com>
2021-07-16 01:54:28 -0400

    Added environment variable and deploying to heroku.

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const server = require('./api/server');
 const port = process.env.PORT || 5000;
 
 // START YOUR SERVER HERE
-
+server.use('/', (req, res) => {
+  const messageOfTheWeek =
+    process.env.MOTW || 'Waiting on a message for the week!';
+  res.send(`API Server is here to stay! ${messageOfTheWeek}`);
+});
 server.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
 });
